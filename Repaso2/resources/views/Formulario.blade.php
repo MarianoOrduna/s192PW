bdc23bb16497df388f70a38c073b4b3beca292c4
MarianoOrduna <122043964@upq.edu.mx>
2024-10-30 19:00:11 -0600

    Actualizacion de formulario

--- Repaso2/resources/views/Formulario.blade.php
+++ Repaso2/resources/views/Formulario.blade.php
@@ -21,5 +21,41 @@
                     <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
                     <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
                   </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Titulo')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Autor')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Paginas')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Año')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Editorial')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">{{__('Email de Editorial')}} </label>
+                    <input type="text" class="form-control" name="txtnombre" value="{{old('txtnombre')}}">
+                    <small class='fst-italic text-danger'>{{$errors->first('txtnombre')}}</small>
+                  </div>
 </body>
 </html>
